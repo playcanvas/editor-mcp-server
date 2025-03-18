05499c331daad1c163ceaabe9fde5fc34ee6daef
Will Eastcott <will@playcanvas.com>
2025-03-18 20:57:21 +0000

    Add new modify_entity tool

--- src/tools/entity.ts
+++ src/tools/entity.ts
@@ -37,6 +37,39 @@ export const register = (server: McpServer, wss: WSS) => {
     );
 
     server.tool(
+        'modify_entity',
+        'Modify an entity\'s properties',
+        {
+            id: z.string(),
+            name: z.string().optional(),
+            enabled: z.boolean().optional(),
+            position: z.array(z.number()).length(3).optional(),
+            rotation: z.array(z.number()).length(3).optional(),
+            scale: z.array(z.number()).length(3).optional(),
+            tags: z.array(z.string()).optional()
+        },
+        async (options) => {
+            try {
+                const res = await wss.send('entity:modify', options.id,options);
+                return {
+                    content: [{
+                        type: 'text',
+                        text: `Modified entity ${options.id}: ${JSON.stringify(res)}`
+                    }]
+                };
+            } catch (err: any) {
+                return {
+                    content: [{
+                        type: 'text',
+                        text: `Failed to modify entity: ${err.message}`
+                    }],
+                    isError: true
+                };
+            }
+        }
+    );
+
+    server.tool(
         'delete_entity',
         'Delete an entity',
         {
@@ -81,90 +114,6 @@ export const register = (server: McpServer, wss: WSS) => {
                     content: [{
                         type: 'text',
                         text: `Failed to list entities: ${err.message}`
-                    }],
-                    isError: true
-                };
-            }
-        }
-    );
-
-    server.tool(
-        'set_entity_position',
-        'Set the position of an entity',
-        {
-            id: z.string(),
-            position: z.array(z.number()).length(3)
-        },
-        async ({ id, position }) => {
-            try {
-                const res = await wss.send('entity:position:set', id, position);
-                return {
-                    content: [{
-                        type: 'text',
-                        text: `Set position of entity ${id} to ${JSON.stringify(res)}`
-                    }]
-                };
-            } catch (err: any) {
-                return {
-                    content: [{
-                        type: 'text',
-                        text: `Failed to set position of entity ${id}: ${err.message}`
-                    }],
-                    isError: true
-                };
-            }
-        }
-    );
-
-    server.tool(
-        'set_entity_rotation',
-        'Set the rotation of an entity',
-        {
-            id: z.string(),
-            rotation: z.array(z.number()).length(3)
-        },
-        async ({ id, rotation }) => {
-            try {
-                const res = await wss.send('entity:rotation:set', id, rotation);
-                return {
-                    content: [{
-                        type: 'text',
-                        text: `Set rotation of entity ${id} to ${JSON.stringify(res)}`
-                    }]
-                };
-            } catch (err: any) {
-                return {
-                    content: [{
-                        type: 'text',
-                        text: `Failed to set rotation of entity ${id}: ${err.message}`
-                    }],
-                    isError: true
-                };
-            }
-        }
-    );
-
-    server.tool(
-        'set_entity_scale',
-        'Set the scale of an entity',
-        {
-            id: z.string(),
-            scale: z.array(z.number()).length(3)
-        },
-        async ({ id, scale }) => {
-            try {
-                const res = await wss.send('entity:scale:set', id, scale);
-                return {
-                    content: [{
-                        type: 'text',
-                        text: `Set scale of entity ${id} to ${JSON.stringify(res)}`
-                    }]
-                };
-            } catch (err: any) {
-                return {
-                    content: [{
-                        type: 'text',
-                        text: `Failed to set scale of entity ${id}: ${err.message}`
                     }],
                     isError: true
                 };
