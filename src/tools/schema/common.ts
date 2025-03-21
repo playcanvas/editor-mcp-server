476fffc0dcd4c34d773b4f52172f2f01d3256667
Will Eastcott <willeastcott@gmail.com>
2025-03-21 12:04:22 +0000

    Improve remaining component schema (#31)

--- src/tools/schema/common.ts
+++ src/tools/schema/common.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
+const Vec2Schema = z.tuple([z.number(), z.number()]).describe('An array of 2 numbers');
 const Vec3Schema = z.tuple([z.number(), z.number(), z.number()]).describe('An array of 3 numbers');
 const Vec4Schema = z.tuple([z.number(), z.number(), z.number(), z.number()]).describe('An array of 4 numbers');
 
-export { Vec3Schema, Vec4Schema };
+export { Vec2Schema, Vec3Schema, Vec4Schema };
