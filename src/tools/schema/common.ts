f581e29bb4c0318b354982c9bbd30a8ce398e48d
Will Eastcott <will@playcanvas.com>
2025-04-06 21:51:33 +0100

    Add newlines

--- src/tools/schema/common.ts
+++ src/tools/schema/common.ts
@@ -5,21 +5,25 @@ const RgbSchema = z.tuple([
     z.number().min(0).max(1).describe('Green'),
     z.number().min(0).max(1).describe('Blue')
 ]).describe('A 3-channel RGB color');
+
 const RgbaSchema = z.tuple([
     z.number().min(0).max(1).describe('Red'),
     z.number().min(0).max(1).describe('Green'),
     z.number().min(0).max(1).describe('Blue'),
     z.number().min(0).max(1).describe('Alpha')
 ]).describe('A 4-channel RGBA color');
+
 const Vec2Schema = z.tuple([
     z.number().describe('X'),
     z.number().describe('Y')
 ]).describe('A 2D vector');
+
 const Vec3Schema = z.tuple([
     z.number().describe('X'),
     z.number().describe('Y'),
     z.number().describe('Z')
 ]).describe('A 3D vector');
+
 const Vec4Schema = z.tuple([
     z.number().describe('X'),
     z.number().describe('Y'),
