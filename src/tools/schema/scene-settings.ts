0baf7c454f8bfea327ce93a586750a328cb5603c
Will Eastcott <will@playcanvas.com>
2025-04-06 19:05:06 +0100

    Lint fix

--- src/tools/schema/scene-settings.ts
+++ src/tools/schema/scene-settings.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
-import { RgbSchema, Vec3Schema } from './common';
 import { AssetIdSchema } from './asset';
+import { RgbSchema, Vec3Schema } from './common';
 
 const PhysicsSchema = z.object({
     gravity: Vec3Schema.default([0, -9.8, 0]).describe('An array of 3 numbers that represents the gravity force.')
