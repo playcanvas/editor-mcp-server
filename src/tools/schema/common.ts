9cb4b0a7709f91011de6fe100dd5b18ded99c434
Will Eastcott <willeastcott@gmail.com>
2025-04-06 18:13:31 +0100

    Refactor common schema (#55)
    * Refactor common schema
    
    * Lint fix
    
    * Leverage AssetIdSchema

--- src/tools/schema/common.ts
+++ src/tools/schema/common.ts
@@ -1,7 +1,30 @@
 import { z } from 'zod';
 
-const Vec2Schema = z.tuple([z.number(), z.number()]).describe('An array of 2 numbers');
-const Vec3Schema = z.tuple([z.number(), z.number(), z.number()]).describe('An array of 3 numbers');
-const Vec4Schema = z.tuple([z.number(), z.number(), z.number(), z.number()]).describe('An array of 4 numbers');
+const RgbSchema = z.tuple([
+    z.number().min(0).max(1).describe('Red'),
+    z.number().min(0).max(1).describe('Green'),
+    z.number().min(0).max(1).describe('Blue')
+]).describe('A 3-channel RGB color');
+const RgbaSchema = z.tuple([
+    z.number().min(0).max(1).describe('Red'),
+    z.number().min(0).max(1).describe('Green'),
+    z.number().min(0).max(1).describe('Blue'),
+    z.number().min(0).max(1).describe('Alpha')
+]).describe('A 4-channel RGBA color');
+const Vec2Schema = z.tuple([
+    z.number().describe('X'),
+    z.number().describe('Y')
+]).describe('A 2D vector');
+const Vec3Schema = z.tuple([
+    z.number().describe('X'),
+    z.number().describe('Y'),
+    z.number().describe('Z')
+]).describe('A 3D vector');
+const Vec4Schema = z.tuple([
+    z.number().describe('X'),
+    z.number().describe('Y'),
+    z.number().describe('Z'),
+    z.number().describe('W')
+]).describe('A 4D vector');
 
-export { Vec2Schema, Vec3Schema, Vec4Schema };
+export { RgbSchema, RgbaSchema, Vec2Schema, Vec3Schema, Vec4Schema };
