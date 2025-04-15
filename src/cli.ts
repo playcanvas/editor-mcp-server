30da4df00e88e736dd7caf3c00145e834aa3dbe3
kpal <kpal81xd@gmail.com>
2025-04-15 13:06:48 +0100

    Cleaned up cli menu

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,50 @@
 #!/usr/bin/env tsx
 
 import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
+
+import pkg from '../package.json';
+
+const options = [
+    {
+        name: 'help',
+        alias: 'h',
+        type: Boolean,
+        defaultValue: false
+    },
+    {
+        name: 'version',
+        alias: 'v',
+        type: Boolean,
+        defaultValue: false
+    },
+    {
+        name: 'port',
+        alias: 'p',
+        type: Number,
+        defaultValue: false
+    }
+];
+
+const help = commandLineUsage([{
+    header: 'Usage',
+    content: `npx ${pkg.name} [options]`
+}, {
+    header: 'Options',
+    optionList: options
+}]);
 
 const main = async (argv: string[]) => {
-    const args = commandLineArgs([
-        { name: 'port', alias: 'p', type: Number, defaultValue: 52000 }
-    ], { argv });
+    const args = commandLineArgs(options, { argv });
+    if (args.help) {
+        console.log(help);
+        return;
+    }
+    if (args.version) {
+        console.log(`v${pkg.version}`);
+        return;
+    }
+
     process.env.PORT = args.port.toString();
     await import('./server');
 };
