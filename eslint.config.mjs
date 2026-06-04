7199c99e4d1a41cb33604005128329a565c39954
kpal <kpal81xd@gmail.com>
2026-06-04 15:36:26 +0100

    Migrate to @playcanvas/eslint-config v3 (/typescript)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,16 @@
-import playcanvasConfig from '@playcanvas/eslint-config';
-import typescriptParser from '@typescript-eslint/parser';
+import typescriptConfig from '@playcanvas/eslint-config/typescript';
 import globals from 'globals';
 
 export default [
-    ...playcanvasConfig,
+    ...typescriptConfig,
     {
         files: ['**/*.ts', '**/*.mjs', '**/*.js'],
         languageOptions: {
-            ecmaVersion: 2022,
-            sourceType: 'module',
-            parser: typescriptParser,
-            parserOptions: {
-                requireConfigFile: false
-            },
             globals: {
                 ...globals.browser,
                 ...globals.mocha,
                 ...globals.node,
                 ...globals.webextensions
-            }
-        },
-        settings: {
-            'import/resolver': {
-                typescript: {
-                    alwaysTryTypes: true,
-                    project: './tsconfig.json'
-                }
             }
         }
     }
