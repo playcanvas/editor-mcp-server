54cd5b35c1b2ed6af123483ab0409e9aa331035a
KPal <kpal81xd@gmail.com>
2026-06-30 10:43:08 +0100

    chore: simplify eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,3 @@
 import typescriptConfig from '@playcanvas/eslint-config/typescript';
-import globals from 'globals';
 
-export default [
-    ...typescriptConfig,
-    {
-        files: ['**/*.ts', '**/*.mjs', '**/*.js'],
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.mocha,
-                ...globals.node,
-                ...globals.webextensions
-            }
-        }
-    }
-];
+export default typescriptConfig;
